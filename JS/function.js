a0b4c7e43d971bd2e5d8bfd0df1b28d661189beb
Anthony Magalhães <anthonymagalhaesacosta@gmail.com>
2023-11-06 11:11:34 -0300

    lessgo

--- JS/function.js
+++ JS/function.js
@@ -1,3 +1,23 @@
-function PagInicial(){
-    location.href = "index.html"
-}   
+
+const form = document.getElementById("form")
+const campos = document.querySelectorAll(".required")
+const spans = document.querySelectorAll(".span-required")
+const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+
+
+function setError(index){
+    campos[index].style.border = "3px solid #e63636 !important"
+    spans[index].style.display = "block"
+}
+function removeError(index){
+    campos[index].style.border = ""
+    spans[index].style.display = "none"
+}
+
+function nomeValidar(){
+    if(campos[0].value.length < 3){
+          setError(0)
+    }else{
+    console.log("Validado o Nome")
+}
+}
